4fdc728fd8071cd1561b4141db79f93da1885426
Andrew Slattery <andrew@hivemetric.com>
2019-12-04 12:34:37 -0700

    Update Fonts
    CAT RCKMS currently uses Open Sans, so shall this.

--- src/themes/default/typography.js
+++ src/themes/default/typography.js
@@ -3,16 +3,16 @@ export default {
         /**
          * @property {string} display for headings and such
          */
-        display: `"Roboto", "SF UI Display", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`,
+        display: `"Open Sans", "SF UI Display", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`,
 
         /**
          * @property {string} ui (default) for text and UI components
          */
-        ui: `"Roboto", "SF UI Text", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`,
+        ui: `"Open Sans", "SF UI Text", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`,
 
         /**
          * @property {string} mono when monospace is bae
          */
-        mono: `"Roboto Mono", "SF Mono", "Monaco", "Inconsolata", "Fira Mono", "Droid Sans Mono", "Source Code Pro", monospace`
+        mono: `"SF Mono", "Monaco", "Inconsolata", "Fira Mono", "Droid Sans Mono", "Source Code Pro", monospace`
     }
 };
