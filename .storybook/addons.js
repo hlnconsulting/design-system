e792b7b694d61c1ec23cab67fb814d7b232c597f
Andrew Slattery <andrew@hivemetric.com>
2019-12-18 14:02:47 -0700

    Fix plugin registration path
    Per release notes for 1.3.0: https://github.com/echoulen/storybook-addon-styled-component-theme/releases/tag/v1.3.0

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -4,4 +4,4 @@ import '@storybook/addon-a11y/register';
 import '@storybook/addon-storysource/register';
 import '@storybook/addon-viewport/register';
 import '@storybook/addon-knobs/register';
-import 'storybook-addon-styled-component-theme/dist/src/register';
+import 'storybook-addon-styled-component-theme/dist/register';
