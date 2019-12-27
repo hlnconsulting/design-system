32c053e8e0b95ab1c439423eeb576db12eebf900
Charlie Maxwell <staxwelljones@gmail.com>
2019-12-27 14:18:17 -0700

    adding dialog element exports

--- src/elements/index.js
+++ src/elements/index.js
@@ -8,6 +8,9 @@ import { TableBody } from './TableBody';
 import { TableCell } from './TableCell';
 import { TableHeader } from './TableHeader';
 import { TableRow } from './TableRow';
+import { DialogHeader } from './DialogHeader';
+import { DialogFooter } from './DialogFooter';
+import { DialogBody } from './DialogBody';
 
 export {
     Button,
@@ -19,5 +22,8 @@ export {
     TableBody,
     TableCell,
     TableHeader,
-    TableRow
+    TableRow,
+    DialogHeader,
+    DialogFooter,
+    DialogBody
 };
