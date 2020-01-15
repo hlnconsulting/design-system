b82a1092e56818894e34314a3e3bd992056ee41e
Andrew Slattery <andrew@hivemetric.com>
2020-01-14 17:05:56 -0700

    Init Control Deck file

---
+++ src/elements/DataTableControlDeck.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+export const DataTableControlDeck = ({ ...props }) => {
+    return <div />;
+};
+
+DataTableControlDeck.propTypes = {};
+
+DataTableControlDeck.defaultProps = {};
